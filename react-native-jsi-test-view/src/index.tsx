62be2e117a529d2cccdc602d62e2d653c1a3b6f7
Stephen <stepan.kopylov@ibitcy.com>
2021-11-30 19:27:35 +0200

    some fixes

--- react-native-jsi-test-view/src/index.tsx
+++ react-native-jsi-test-view/src/index.tsx
@@ -1,9 +1,35 @@
-import { requireNativeComponent, ViewProps } from "react-native";
-import * as React from 'react';
+import { requireNativeComponent, ViewProps, findNodeHandle } from "react-native";
+import * as React from "react";
+import { Ref, useImperativeHandle, useMemo } from "react";
 
 export const JsiTestView = requireNativeComponent("JsiTestView");
 
-export const JsiTestViewComponent: React.FC<ViewProps> = (props) => {
+export interface IJsiTestViewComponentRef {
+  foo: () => void;
+  bar: () => void;
+}
 
-  return (<JsiTestView {...props}></JsiTestView>);
-};
+export const JsiTestViewComponent: React.FC<ViewProps> = React.forwardRef(
+  (props, ref?: Ref<IJsiTestViewComponentRef | any>) => {
+    const viewRef = React.useRef<any>(null);
+
+    const handle = useMemo(() => {
+      return findNodeHandle(viewRef.current);
+    }, [viewRef.current]);
+
+    useImperativeHandle(
+      ref,
+      () => ({
+        foo: () => {
+          // const i = cloneIndicatorFields(indicator);
+          // NativeModules.RNExpertOptionMobilePlot.addIndicator(handle, i);
+        },
+        bar: () => {
+          // NativeModules.RNExpertOptionMobilePlot.removeIndicator(handle, index);
+        }
+      }),
+      [handle]
+    );
+    return <JsiTestView {...props} ref={viewRef}></JsiTestView>;
+  }
+);
